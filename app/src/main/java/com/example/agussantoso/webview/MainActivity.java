a6f607c08452bca811d37d2c847fcbdf9b0867f2
aguscoakep <aguscoakep@gmail.com>
2018-10-24 09:54:26 +0700

    WebViewAdroid

---
+++ app/src/main/java/com/example/agussantoso/webview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.agussantoso.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://www.okedroid.com");
+    }
+}
